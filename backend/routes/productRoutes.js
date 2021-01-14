346c3171a290819b8cfe26e6b6dd048abea4cf70
irwantowibowotech <irwantowibowotech@gmail.com>
2021-01-14 23:49:15 +0700

    menhubungkan FE denan redux yang kita setting

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,6 +10,10 @@ router.get(
   "/",
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
+
+    // res.status(401);
+    // throw new Error("Not Authorize");
+
     res.json(products);
   })
 );
