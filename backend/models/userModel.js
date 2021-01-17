a665b200f59c11217244d725beb1fb1e9e4386a6
irwantowibowotech <irwantowibowotech@gmail.com>
2021-01-17 15:33:02 +0700

    membuat user authentication (register dan login)

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
@@ -23,6 +24,21 @@ const userSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Melakukan enkripsi password sebelum data disimpan (pre artinya sebelum)
+userSchema.pre("save", async function (next) {
+  // Jika password tidak diperbaharui (data lain diupdate --> biasanya saat update data)
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password); //this.password adalah kolom password (tabel)
+};
 
 const User = mongoose.model("User", userSchema);
 
